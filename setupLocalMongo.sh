3aec637a1ba001c4602f49a80df73db30b04aa83
rajendra <30436194+luktumun@users.noreply.github.com>
2025-01-24 19:29:05 +0530

    Update setupLocalMongo.sh

--- setupLocalMongo.sh
+++ setupLocalMongo.sh
@@ -1,3 +1,8 @@
 # Setup file to upload data to MongoDB 
-mongo xflix --eval "db.dropDatabase()" 
-mongoimport -d xflix -c videos --file data/VideoData.json
+# mongo xflix --eval "db.dropDatabase()" 
+# mongoimport -d xflix -c videos --file data/VideoData.json
+
+
+
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb://ac-mpp1kok-shard-00-00.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-01.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-02.phvwerh.mongodb.net:27017/wargame?replicaSet=atlas-yrs1tu-shard-0" --ssl --authenticationDatabase admin --username srajendra923 --password R30LK8ikcQbqrcPZ  --drop --collection videos --file data/VideoData.json --jsonArray
